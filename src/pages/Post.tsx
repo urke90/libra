0c2d9a440f4b68fc72fc8f791fba0d2754dc873e
urke90 <urosbijelic90@gmail.com>
2022-11-19 12:20:11 +0100

    init commit project setup

---
+++ src/pages/Post.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './Post.scss';
+
+interface IPostProps {}
+
+const Post: React.FC<IPostProps> = () => {
+    return <div className="post">Single post page</div>;
+};
+export default Post;
---
+++ src/pages/Posts.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './Posts.scss';
+
+interface IPostsProps {}
+
+const Posts: React.FC<IPostsProps> = () => {
+    return <div className="posts">Posts page</div>;
+};
+export default Posts;
